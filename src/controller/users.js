7e750ed315f6439012659ef4e33a5313fbd98f55
DimasNuryadin <dimasnuryadinn@gmail.com>
2023-01-06 21:37:11 +0700

    add jwt

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,14 +1,25 @@
 const UsersModels = require('../models/users')
+const jwt = require('jsonwebtoken');
+const config = require('../config/jwt');
 
 const loginUser = async (req, res) => {
   const { body } = req;
 
   try {
     const [data] = await UsersModels.loginUser(body);
+    // Data destructuring
+    const data1 = data[0];
+    const token = jwt.sign(data1, config.secret);
+
     if (data.length === 1) {
       res.status(200).json({
         message: 'LOGIN users success',
-        data: body
+        data: {
+          id: data1.id,
+          username: data1.username,
+          password: data1.password,
+          token: token
+        },
       })
     } else {
       res.status(401).json({
