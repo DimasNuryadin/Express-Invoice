28ff66cf6b1ee7bf1939060a64afb2171833fdfb
DimasNuryadin <dimasnuryadinn@gmail.com>
2023-01-09 18:59:50 +0700

    refactor create invoices

--- src/models/invoices.js
+++ src/models/invoices.js
@@ -1,5 +1,13 @@
 const dbPool = require('../config/database')
 
+const today = new Date();
+const date = today.getDate();
+const month = today.getMonth();
+const year = today.getFullYear();
+
+const now = `${year}-${month + 1}-${date}`;
+// console.log("now : ", now)
+
 const getAllInvoices = () => {
   const SQLQuery = 'SELECT * FROM invoices';
   return dbPool.execute(SQLQuery);
@@ -12,20 +20,12 @@ const getInvoices = (idInvoices) => {
 }
 
 const createNewInvoices = (body) => {
-  const SQLQuery = `INSERT INTO invoices (alamat_perusahaan, no_invoice, company, invoice_date, due_date, payment_instruction) VALUES ('${body.alamat_perusahaan}', '${body.no_invoice}', '${body.company}', '${body.invoice_date}', '${body.due_date}', '${body.payment_instruction}')`;
+  const SQLQuery = `INSERT INTO invoices (alamat_perusahaan, no_invoice, company, invoice_date, due_date, latest_update) VALUES ('${body.alamat_perusahaan}', '${body.no_invoice}', '${body.company}', '${body.invoice_date}', '${body.due_date}', '${now}')`;
 
   return dbPool.execute(SQLQuery)
 }
 
 const updateInvoiceStep1 = (body, idInvoices) => {
-  const today = new Date();
-  const date = today.getDate();
-  const month = today.getMonth();
-  const year = today.getFullYear();
-
-  const now = `${year}-${month + 1}-${date}`;
-  // console.log("now : ", now)
-
   const SQLQuery = `UPDATE invoices SET 
                     alamat_perusahaan='${body.alamat_perusahaan}', 
                     no_invoice='${body.no_invoice}', 
@@ -39,14 +39,6 @@ const updateInvoiceStep1 = (body, idInvoices) => {
 }
 
 const updateInvoiceStep2 = (body, idInvoices) => {
-  const today = new Date();
-  const date = today.getDate();
-  const month = today.getMonth();
-  const year = today.getFullYear();
-
-  const now = `${year}-${month + 1}-${date}`;
-  // console.log("now : ", now)
-
   const SQLQuery = `UPDATE invoices SET 
                     payment_instruction='${body.payment_instruction}', 
                     discount='${body.discount}', 
