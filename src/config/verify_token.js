76f145376b9d202dd061aec0d812af091a90a00f
DimasNuryadin <dimasnuryadinn@gmail.com>
2023-02-28 19:02:26 +0700

    install express-jwt

--- src/config/verify_token.js
+++ src/config/verify_token.js
@@ -1,7 +1,27 @@
 const jwt = require('jsonwebtoken');
+const config = require('./secret_jwt');
 
-const verifyToken = () => {
+const verifyToken = (req, res) => {
+  const authHeader = req.headers['authorization'];
 
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) {
+    return res.status(401).json({
+      message: 'Anda belum login',
+      error: 'Unauthorized'
+    });
+  }
+
+  jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+
+    req.user = decoded.user;
+    console.log('decoded token : ', req.user)
+    next();
+  })
 }
 
 module.exports = verifyToken;
--- src/routes/invoices.js
+++ src/routes/invoices.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router();
 const InvoiceController = require('../controller/invoices')
+const verifyToken = require('../config/verify_token');
 
-router.get('/', InvoiceController.getAllInvoices) // GET ALL invoices
+router.get('/', verifyToken, InvoiceController.getAllInvoices) // GET ALL invoices
 router.get('/:idInvoices', InvoiceController.getInvoices) // GET invoices by id
 router.get('/company/:company', InvoiceController.getInvoicesCompany) // GET invoices by company
 router.post('/', InvoiceController.createNewInvoices)  // CREATE invoices
